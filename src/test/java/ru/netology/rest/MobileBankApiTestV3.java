92c49fb82b40f0b5f3fd7ce29efaabcf8b87781b
AntonI <avmiass@bk.ru>
2023-03-10 18:51:36 +0300

    added schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -24,7 +24,63 @@ class MobileBankApiTestV3 {
           .contentType(ContentType.JSON)
           .body("", hasSize(3))
           .body("[0].currency", equalTo("RUB"))
+          .body("[2].currency", equalTo("RUB"))
           .body("[0].balance", greaterThanOrEqualTo(0))
       ;
+    }
+
+    @Test
+    void shouldFirstCurrencyNotMatch() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .body("[2].currency", equalTo("RUB"))
+                .body("[0].currency", equalTo("RUB"))
+        ;
+    }
+
+    @Test
+    void shouldSecondCurrencyNotMatch() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .body("[0].currency", equalTo("RUB"))
+                .body("[2].currency", equalTo("RUB"))
+        ;
+    }
+
+    @Test
+    void shouldCurrencyUsd() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[1].currency", equalTo("USD"))
+        ;
     }
 }
