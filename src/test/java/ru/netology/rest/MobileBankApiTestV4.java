04fb984d57cf52c5989f23ec2b04f6c519373aec
AntonI <avmiass@bk.ru>
2023-03-15 12:34:08 +0300

    edit added

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -1,9 +1,14 @@
 package ru.netology.rest;
 
+import io.restassured.specification.Argument;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 
 class MobileBankApiTestV4 {
     @Test
@@ -20,5 +25,21 @@ class MobileBankApiTestV4 {
           .statusCode(200)
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
       ;
+    }
+
+    @Test
+    void shouldAllCurrency() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
     }
 }
